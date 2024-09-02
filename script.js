d2d9b1a42bdf2baf9b1ddf00024a5dbe96806a08
Developer Tonny <developertonny.info@gmail.com>
2024-09-02 16:13:40 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// faq
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+
+
+
+// date & time
+function updateTime() {
+    const now = new Date();
+    const date = now.toLocaleDateString();
+    const time = now.toLocaleTimeString();
+
+    document.getElementById('date').textContent = date;
+    document.getElementById('time').textContent = time;
+}
+
+setInterval(updateTime, 1000);
+updateTime(); // initial call to display the time immediately
